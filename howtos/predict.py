2a288fd0f5e093f0b950cadda3224afc50e57be5
Pedro Leitao <pedro.leitao@yahoo.com>
2024-04-07 12:35:11 +0100

    Changed license.

--- howtos/predict.py
+++
@@ -1,8 +0,0 @@
-
-import tensorflow as tf
-from model_setup import predict, plot_dataset_and_predictions
-
-saved_model = tf.keras.models.load_model("trained_model.keras")
-saved_dataset = tf.data.Dataset.load("dataset.keras", element_spec=None)
-pred_x, pred_y = predict(saved_model, 100)
-plot_dataset_and_predictions(saved_dataset, pred_x, pred_y)
--- howtos/runner.py
+++
@@ -1,9 +0,0 @@
-
-import tensorflow as tf
-from worker import worker_setup, run_worker
-
-worker_setup()
-trained_model, dataset = run_worker()
-print("Worker finished...")
-trained_model.save("trained_model.keras")
-tf.data.Dataset.save(dataset, "dataset.keras")
